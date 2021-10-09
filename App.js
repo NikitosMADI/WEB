2a3c4bf3c082ebd786ad159bfb413eda53300b28
NikitosMADI <66263671+NikitosMADI@users.noreply.github.com>
2021-10-09 20:47:16 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import NoteList from "./components/NoteList";
+
+function App() {
+  return (
+    <div className="note-app">
+      <h1>РЕЦЕПТЫ</h1>
+      <NoteList />
+    </div>
+  );
+}
+
+export default App;
